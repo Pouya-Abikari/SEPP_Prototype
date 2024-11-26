fac82d650f14afca55feb246594570d6884d1af1
nazi2004 <nxm202@student.bham.ac.uk>
2024-11-26 14:46:30 +0400

    nazanin 009

--- src/main/java/se_prototype/se_prototype/MenuController.java
+++ src/main/java/se_prototype/se_prototype/MenuController.java
@@ -76,6 +76,8 @@ public class MenuController {
         homeButton.setOnAction(event -> switchToPage("home_screen.fxml", "Home"));
         menuButton.setOnAction(event -> switchToPage("menu.fxml", "Menu"));
         settingsButton.setOnAction(event -> switchToPage("settings.fxml", "Settings"));
+        cartButton.setOnAction(event -> switchToPage("cart.fxml", "Cart"));
+
     }
 
     private void setupImages() {
