b8eec42a670b10e5b9858658f4dac38817ae637d
Pouya <pouyaabikari@gmail.com>
2024-11-29 12:50:23 +0400

    Pouya-016

--- src/main/java/se_prototype/se_prototype/StartGroupOrderController.java
+++ src/main/java/se_prototype/se_prototype/StartGroupOrderController.java
@@ -1,11 +1,19 @@
 package se_prototype.se_prototype;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartGroupOrderController {
+
+    @FXML
+    private Button startGroupOrderButton;
 
     @FXML
     private Button backButton;
@@ -33,5 +41,23 @@ public class StartGroupOrderController {
         notificationIcon.setImage(new Image(getClass().getResourceAsStream("/icons/notification.png")));
         cartIcon.setImage(new Image(getClass().getResourceAsStream("/icons/cart.png")));
         itemsIcon.setImage(new Image(getClass().getResourceAsStream("/icons/items.png")));
+
+        startGroupOrderButton.setOnAction(e -> switchToPage("groupOrder.fxml", "Group Order"));
+        backButton.setOnAction(e -> switchToPage("cart.fxml", "Cart"));
+    }
+
+    private void switchToPage(String fxmlFile, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Scene scene = new Scene(loader.load(), 400, 711);
+            scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+            Stage stage = (Stage) backButton.getScene().getWindow(); // Get the current stage
+            stage.setScene(scene);
+            stage.setTitle(title);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Failed to load " + fxmlFile + ": " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
